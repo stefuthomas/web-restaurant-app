8a0ce27cc7bbe2527adc9907a5c1db26f046ff08
stefu <stefanos.thomas@metropolia.fi>
2024-04-13 14:35:11 +0300

    applied a color pallet for the css

--- layout/scripts/map.js
+++ layout/scripts/map.js
@@ -27,7 +27,7 @@ function createMap() {
 
 const userLocationIcon = new L.Icon({
   iconUrl:
-    "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png",
+    "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-grey.png",
   shadowUrl:
     "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
   iconSize: [25, 41],
@@ -38,7 +38,7 @@ const userLocationIcon = new L.Icon({
 
 const restaurantLocationIcon = new L.Icon({
   iconUrl:
-    "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-orange.png",
+    "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-black.png",
   shadowUrl:
     "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
   iconSize: [25, 41],
